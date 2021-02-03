72a7ef945686ea343d674564d1c8b85e7af42d7d
Jack Harvey <jackrrv@gmail.com>
2021-02-03 01:03:29 +0000

    vim: move to coc.nvim and Plug for plugins

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# Get current dir (so run this script from anywhere)
+# Install Plugged
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# Get current dir (so run this script from anywhere)
 export DOTFILES_DIR EXTRA_DIR
+
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXTRA_DIR="$HOME/.extra"
 
